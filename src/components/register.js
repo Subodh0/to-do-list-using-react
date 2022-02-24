a6351bc04734609b188fa608683a28cc2341a5da
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2022-02-24 12:52:56 +0530

    added classbased component in register.js file

--- src/components/register.js
+++ src/components/register.js
@@ -1,36 +1,58 @@
 import { useState } from "react";
 import axios from "axios";
-import Header from "./header";
+import { Navigate } from "react-router-dom";
+import { Component } from "react";
 
-export default function RegisterUser () {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [age, setAge] = useState();
-    const [userName, setUserName] = useState("");
-    const [apiMessage, setAPIMessage] = useState();
 
-    const registerUser = () =>{
-        axios.post('https://api-nodejs-todolist.herokuapp.com/user/register',{
-            email : email,
-            password : password,
-            name : userName,
-            age : parseInt(age)
-        }).then((res)=>{
-            setAPIMessage({
-                flag: 0,
-                message : "Registration successful !!!"
+export default class RegisterUserComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email : "",
+            password : "",
+            userName : "",
+            disableButton : false
+        }
+    }
+
+    registerUser = async () => {
+        this.setState({
+            disableButton : true
+        })
+
+        await axios.post('https://api-nodejs-todolist.herokuapp.com/user/register',{
+            email : this.state.email,
+            password : this.state.password,
+            name : this.state.userName,
+            age : parseInt(this.state.age)
+        }).then((res) => {
+            this.setState({
+                flag : 0,
+                message : "Registration successfull !!"
             })
-        }).catch((error)=>{
-            if(error && error.response && error.response.data)
-                setAPIMessage({
-                    flag:1,
-                    message:error.response.data
+        }).catch((err) => {
+            if(err && err.response && err.response.data)
+                this.setState({
+                    flag : 1,
+                    message : err.response.data,
+                    email : "",
+                    password : "",
+                    userName : "",
+                    age : undefined
                 })
+        })
+        this.setState({
+            disableButton : false
         })
     }
 
-    return (
-        <section>
+    render() {
+        if(this.state.flag === 0)
+            return (<Navigate to="/" />)
+
+        return (
+            <section>
             <div className="mt-5 row">
                 <h1 className="font-weight-bold w-100">My To-Do List Application</h1>
             </div>
@@ -52,10 +74,10 @@ export default function RegisterUser () {
                                 </div>
                                 <input 
                                     type="text" 
-                                    value = {userName} 
+                                    value = {this.state.userName} 
                                     className="form-control" 
                                     placeholder="name" 
-                                    onChange = {(e) => {setUserName(e.target.value)}} 
+                                    onChange = {(e) => {this.setState({userName : e.target.value})}} 
                                 />
                             </div>
                             <div className="input-group form-group">
@@ -64,10 +86,10 @@ export default function RegisterUser () {
                                 </div>
                                 <input 
                                     type="text" 
-                                    value = {age} 
+                                    value = {this.state.age} 
                                     className="form-control" 
                                     placeholder="age" 
-                                    onChange = {(e) => {setAge(e.target.value)}} 
+                                    onChange = {(e) => {this.setState({age : e.target.value})}} 
                                 />
                             </div>
                             <div className="input-group form-group">
@@ -76,10 +98,10 @@ export default function RegisterUser () {
                                 </div>
                                 <input 
                                     type="text" 
-                                    value = {email} 
+                                    value = {this.state.email} 
                                     className="form-control" 
                                     placeholder="email" 
-                                    onChange = {(e) => {setEmail(e.target.value)}} 
+                                    onChange = {(e) => {this.setState({email : e.target.value})}} 
                                 />
                             </div>
                             <div className="input-group form-group">
@@ -88,19 +110,20 @@ export default function RegisterUser () {
                                 </div>
                                 <input 
                                     type="password" 
-                                    value = {password} 
+                                    value = {this.state.password} 
                                     className="form-control" 
                                     placeholder="password" 
-                                    onChange = {(e) => {setPassword(e.target.value)}}
+                                    onChange = {(e) => {this.setState({password : e.target.value})}}
                                 />
                             </div>
-                            {(apiMessage) &&
-                                <p className={!apiMessage.flag ? "text-success" : "text-danger"}>{apiMessage.message}</p>
+                            {(this.state.apiMessage) &&
+                                <p className={!this.state.apiMessage.flag ? "text-success" : "text-danger"}>{this.state.apiMessage.message}</p>
                             }
                             <div className="form-group">
                                 <button 
                                     className="btn btn-primary float-right login_btn"
-                                    onClick={() => {registerUser()}}
+                                    onClick={() => {this.registerUser()}}
+                                    disabled = {this.state.disableButton}
                                 >
                                     Register
                                 </button>
@@ -114,6 +137,124 @@ export default function RegisterUser () {
                     </div>
                 </div>
             </div>
-        </section>    
-    )
+        </section>
+        );
+    }
+
 }
+
+// export function RegisterUser () {
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [age, setAge] = useState();
+//     const [userName, setUserName] = useState("");
+//     const [apiMessage, setAPIMessage] = useState();
+
+//     const registerUser = () =>{
+//         axios.post('https://api-nodejs-todolist.herokuapp.com/user/register',{
+//             email : email,
+//             password : password,
+//             name : userName,
+//             age : parseInt(age)
+//         }).then((res)=>{
+//             setAPIMessage({
+//                 flag: 0,
+//                 message : "Registration successful !!!"
+//             })
+//         }).catch((error)=>{
+//             if(error && error.response && error.response.data)
+//                 setAPIMessage({
+//                     flag:1,
+//                     message:error.response.data
+//                 })
+//         })
+//     }
+
+//     return (
+//         <section>
+//             <div className="mt-5 row">
+//                 <h1 className="font-weight-bold w-100">My To-Do List Application</h1>
+//             </div>
+//             <div className="container mt-5">
+//                 <div className="d-flex justify-content-center h-100">
+//                     <div className="card">
+//                         <div className="card-header">
+//                             <h3>Register</h3>
+//                             <div className="d-flex justify-content-end social_icon">
+//                                 <span><i className="fab fa-facebook-square"></i></span>
+//                                 <span><i className="fab fa-google-plus-square"></i></span>
+//                                 <span><i className="fab fa-twitter-square"></i></span>
+//                             </div>
+//                         </div>
+//                         <div className="card-body">
+//                             <div className="input-group form-group">
+//                                 <div className="input-group-prepend">
+//                                     <span className="input-group-text"><i className="fas fa-user"></i></span>
+//                                 </div>
+//                                 <input 
+//                                     type="text" 
+//                                     value = {userName} 
+//                                     className="form-control" 
+//                                     placeholder="name" 
+//                                     onChange = {(e) => {setUserName(e.target.value)}} 
+//                                 />
+//                             </div>
+//                             <div className="input-group form-group">
+//                                 <div className="input-group-prepend">
+//                                     <span className="input-group-text"><i className="fas fa-user"></i></span>
+//                                 </div>
+//                                 <input 
+//                                     type="text" 
+//                                     value = {age} 
+//                                     className="form-control" 
+//                                     placeholder="age" 
+//                                     onChange = {(e) => {setAge(e.target.value)}} 
+//                                 />
+//                             </div>
+//                             <div className="input-group form-group">
+//                                 <div className="input-group-prepend">
+//                                     <span className="input-group-text"><i className="fas fa-user"></i></span>
+//                                 </div>
+//                                 <input 
+//                                     type="text" 
+//                                     value = {email} 
+//                                     className="form-control" 
+//                                     placeholder="email" 
+//                                     onChange = {(e) => {setEmail(e.target.value)}} 
+//                                 />
+//                             </div>
+//                             <div className="input-group form-group">
+//                                 <div className="input-group-prepend">
+//                                     <span className="input-group-text"><i className="fas fa-key"></i></span>
+//                                 </div>
+//                                 <input 
+//                                     type="password" 
+//                                     value = {password} 
+//                                     className="form-control" 
+//                                     placeholder="password" 
+//                                     onChange = {(e) => {setPassword(e.target.value)}}
+//                                 />
+//                             </div>
+//                             {(apiMessage) &&
+//                                 <p className={!apiMessage.flag ? "text-success" : "text-danger"}>{apiMessage.message}</p>
+//                             }
+//                             <div className="form-group">
+//                                 <button 
+//                                     className="btn btn-primary float-right login_btn"
+//                                     onClick={() => {registerUser()}}
+//                                 >
+//                                     Register
+//                                 </button>
+//                             </div>
+//                         </div>
+//                         <div className="card-footer">
+//                             <div className="d-flex justify-content-center links">
+//                                 Don't have an account?<a href="/">Log In</a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>    
+//     )
+// }
